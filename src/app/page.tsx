dd6c8a7b23079e7e28705720e05f0bd2b2abf0ed
Sreta29 <sreta290101@gmail.com>
2024-05-24 16:30:49 +0800

    Edited Welcome Page text for testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { Login, Register } from "./loginregister";
 export default function Welcome() {
   return (
     <>
-      <h1>Welcome Page</h1>
+      <h1>Welcome to Solid Waste Website</h1>
       <ModeToggle />
       <Login />
       <Register />

5e5461cec8bf50d636483beaa573a2e73579d240
THEALIFHAKER1 <alifhaker1@gmail.com>
2024-05-25 12:36:27 +0800

    a proper way to linked pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,7 @@
-import Image from "next/image";
-import { ModeToggle } from "@/components/modetoggleicon";
-import { Login, Register } from "./loginregister";
-
 export default function Welcome() {
   return (
     <>
-      <h1>Welcome to Solid Waste Website</h1>
-      <ModeToggle />
-      <Login />
-      <Register />
+      <h1 className="text-5xl font-bold">Welcome to Solid Waste Website</h1>
     </>
   );
 }
--- src/app/pages/rating.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Notification from "./notification";
-import Payment from "./payment";
-
-function Rating() {
-  return <div>Rating</div>;
-}
-
-export default Rating;
